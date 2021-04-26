e6906f67ebc03786dd5564b00cbf10e2357d63e7
Mariana Costa <marianapatc@hotmail.com>
2021-04-26 18:17:40 +0100

    implement minor fixes

--- src/components/tile/StyledTile.js
+++ src/components/tile/StyledTile.js
@@ -28,13 +28,14 @@ export const StyledTitle = styled.h4``
 
 export const StyledImage = styled.img`
   width: 18rem;
+  height: 11.4rem;
   box-shadow: 0.25rem 0.25rem 0.7rem rgb(0, 0, 0, 0.7);
   border-radius: 0.25rem;
 `
 
 export const StyledImagePlaceholder = styled.div`
   width: 18rem;
-  height: 6.25rem;
+  height: 11.4rem;
 `
 
 export const StyledDescription = styled.p`
